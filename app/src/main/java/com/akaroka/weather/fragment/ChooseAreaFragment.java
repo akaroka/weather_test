d8e37446956b8431004673bc5a88804a9ba29d3d
fanyj.dell <fanyj.hf@hotmail.com>
2020-02-21 22:12:09 +0800

    增加一个碎片fragment，处理CURD问题

--- app/src/main/java/com/akaroka/weather/fragment/ChooseAreaFragment.java
+++ app/src/main/java/com/akaroka/weather/fragment/ChooseAreaFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,9 +18,18 @@ import com.akaroka.weather.R;
 import com.akaroka.weather.db.City;
 import com.akaroka.weather.db.County;
 import com.akaroka.weather.db.Province;
+import com.akaroka.weather.util.HttpUtil;
+import com.akaroka.weather.util.Utility;
 
+import org.litepal.LitePal;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class ChooseAreaFragment extends Fragment{
 
@@ -27,6 +38,8 @@ public class ChooseAreaFragment extends Fragment{
     public static final int LEVEL_CITY = 1;
 
     public static final int LEVEL_COUNTY = 2;
+
+    private static final String API_ADDRESS = "http://guolin.tech/api/china";
 
     private ProgressBar progressBar;
     //todo
@@ -51,7 +64,7 @@ public class ChooseAreaFragment extends Fragment{
 
     private City selectedCity;
 
-    private int cuttentLevel;
+    private int currentLevel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -65,5 +78,142 @@ public class ChooseAreaFragment extends Fragment{
         return view;
 
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated((savedInstanceState));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(currentLevel == LEVEL_PROVINCE){
+                    selectedProvince = provinceList.get(position);
+                    queryCities();
+                } else if(currentLevel == LEVEL_CITY){
+                    selectedCity = cityList.get(position);
+                    queryCounties();
+                }
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentLevel == LEVEL_COUNTY){
+                    queryCities();
+                } else if (currentLevel == LEVEL_CITY){
+                    queryProvinces();
+                }
+
+            }
+        });
+        queryProvinces();
+    }
+
+
+    private void queryFromServer(String address, final String type){
+
+        progressBar.setVisibility(View.VISIBLE);
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable(){
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.GONE);
+                        Toast.makeText(getContext(), "加载失败", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText = response.body().string();
+                boolean result = false;
+                if("province".equals(type)){
+                    result = Utility.handleProvinceResponse(responseText);
+                } else if ("city".equals(type)){
+                    result = Utility.handleCityResponse(responseText, selectedProvince.getId());
+                } else if ("county".equals(type)){
+                    result = Utility.handleCountyResponse(responseText, selectedCity.getId());
+                }
+                if(result){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setVisibility(View.GONE);
+                            if("province".equals(type)){
+                                queryProvinces();
+                            } else if ("city".equals(type)){
+                                queryCities();
+
+                            } else if("county".equals(type))
+                                queryCounties();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void queryProvinces(){
+        titleText.setText("中华人民共和国");
+        backButton.setVisibility(View.GONE);
+        provinceList = LitePal.findAll(Province.class);
+        if(provinceList.size() > 0 ){
+            dataList.clear();
+            for(Province province:provinceList){
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_PROVINCE;
+        } else {
+            String address = API_ADDRESS;
+            queryFromServer(address, "province");
+        }
+
+    }
+
+    private void queryCities(){
+        titleText.setText(selectedProvince.getProvinceName());
+        backButton.setVisibility(View.VISIBLE);
+        cityList = LitePal.where("provinceid=?", String.valueOf(selectedProvince.getProvinceCode())).find(City.class);
+        if(cityList.size() > 0 ){
+            dataList.clear();
+            for(City city:cityList){
+                dataList.add(city.getCityName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_CITY;
+        } else {
+            int provinceCode = selectedProvince.getProvinceCode();
+            String address = API_ADDRESS + provinceCode;
+            queryFromServer(address, "city");
+        }
+
+    }
+
+    private void queryCounties(){
+        titleText.setText(selectedCity.getCityName());
+        backButton.setVisibility(View.VISIBLE);
+        countyList = LitePal.where("cityid = ?", String.valueOf(selectedCity.getCityCode())).find(County.class);
+        if(countyList.size() > 0 ){
+            dataList.clear();
+            for(County county:countyList){
+                dataList.add(county.getCountyName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_COUNTY;
+        } else {
+            int provinceCode = selectedProvince.getProvinceCode();
+            int cityCode = selectedCity.getCityCode();
+            String address = API_ADDRESS + provinceCode + "/" + cityCode;
+            queryFromServer(address, "county");
+        }
+
+    }
+
 
 }
